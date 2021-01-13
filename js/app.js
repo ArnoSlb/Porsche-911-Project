2a73b123b9f07378642f5e271b5fe30a6cfca3df
ArnoSlb <soulabaille.arnaud@gmail.com>
2021-01-13 16:47:57 +0100

    test point light

--- js/app.js
+++ js/app.js
@@ -1,81 +1,101 @@
+function main() {
 
-const container = document.querySelector('.scene');
-// Je cible la balise html qui a pour class "scene"
+    const container = document.querySelector('.scene');
+    // Je cible la balise html qui a pour class "scene"
 
-const scene = new THREE.Scene();
-// Je crée la scène
+    const renderer = new THREE.WebGLRenderer({antialias:true, alpha: true});
+    // Je crée le moteur de rendu
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    // J'indique la taille de l'image que je souhaite rendre
+    renderer.setPixelRatio(window.devicePixelRatio);
+    // Le ratio des pixels s'adaptera à l'appreil sur lequel le site est visualisé
 
-const fov = 55;
-// Je définis le champ de vision de la camera à 75 degrés
-const aspect = container.clientWidth / container.clientHeight
-// Je définis le ratio de la caméra
-const near = 0.1;
-// Je définis la distance la plus petite
-const far = 500;
-// Je définis la distance la plus grande
+    container.appendChild(renderer.domElement);
+  
+    const fov = 45;
+    const aspect = 2;  // the canvas default
+    const near = 0.1;
+    const far = 100;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(0, 3, 30);
+  
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color('black');
 
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-// Je crée la caméra avec les paramètres donnés ci-dessous
-camera.position.set( 0, 1.7, 7 );
-// Je donne une position de départ à la caméra
+      {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.PointLight(color, intensity);
+        light.position.set(0, 10, 0);
+        scene.add(light);
+    
+      }
 
-const light = new THREE.PointLight(0x404040, 40);
-// Je crée une lumiere ambiante, diffuse dans toutes les directions
-// La première valeur correspond à la couleur de la lumière et la deuxième à son intensité
-light.position.set(0,15,0)
-scene.add(light);
-
-const renderer = new THREE.WebGLRenderer({antialias:true, alpha: true});
-// Je crée le moteur de rendu
-renderer.setSize(container.clientWidth, container.clientHeight);
-// J'indique la taille de l'image que je souhaite rendre
-renderer.setPixelRatio(window.devicePixelRatio);
-// Le ratio des pixels s'adaptera à l'appreil sur lequel le site est visualisé
-
-container.appendChild(renderer.domElement);
-// J'injecte le <canvas> créé par three.js dans la balise html ciblé dans container
-
-// CHARGEMENT MODELES 3D
-const loaderPorsche911turbo = new THREE.GLTFLoader();
-// Je crée le loader qui va me permettre de récupérer mon élement 3D
-loaderPorsche911turbo.load('../assets/3D models/free_1975_porsche_911_930_turbo/scene.gltf', function(porsche911turbo){
-    // Je récupère le modèle 3D
-    console.log(porsche911turbo);
-    porsche911turbo.scene.position.x = -3.5;
-    porsche911turbo.scene.rotation.y = .9;
-    scene.add(porsche911turbo.scene);
-    // J'ajoute l'élément récupéré à la scène
-})
-
-const loaderPorsche911carrera = new THREE.GLTFLoader();
-loaderPorsche911carrera.load('../assets/3D models/free_porsche_911_carrera_4s/scene.gltf', function(porsche911carrera){
-    porsche911carrera.scene.position.x = 3.5;
-    porsche911carrera.scene.position.y = .7;
-    porsche911carrera.scene.scale.x = 1.2;
-    porsche911carrera.scene.scale.y = 1.2;
-    porsche911carrera.scene.scale.z = 1.2;
-    porsche911carrera.scene.rotation.y = -.9;
-    scene.add(porsche911carrera.scene);
-})
-
-const animate = function () {
-    requestAnimationFrame(animate);
-    // Je crée une render loop qui va permettre de recalculer la scène 60fois/seconde (sur un écran classique).
-    renderer.render( scene, camera );
-    // Je rends la scène et la caméra ensemble
-};
-
-animate();
-
-function onWindowResize(){
-    camera.aspect = container.clientWidth / container.clientHeight
-    // Je lui demande de recalculer l'aspect de la camera
-    camera.updateProjectionMatrix();
-    //J'appelle une fonction qui s'occupe de tout pour nous
-
-    renderer.setSize(container.clientWidth, container.clientHeight)
-    // Je lui reprécise la taille de l'écran à rendre
-}
-
-window.addEventListener("resize", onWindowResize)
-// A chaque changment de taille, je lance la fonction onWindowResize
+      {
+        const planeSize = 40;
+    
+        const loader = new THREE.TextureLoader();
+        const texture = loader.load('https://threejsfundamentals.org/threejs/resources/images/checker.png');
+        texture.wrapS = THREE.RepeatWrapping;
+        texture.wrapT = THREE.RepeatWrapping;
+        texture.magFilter = THREE.NearestFilter;
+        const repeats = planeSize / 2;
+        texture.repeat.set(repeats, repeats);
+    
+        const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
+        const planeMat = new THREE.MeshPhongMaterial({
+          map: texture,
+          side: THREE.DoubleSide,
+        });
+        const mesh = new THREE.Mesh(planeGeo, planeMat);
+        mesh.rotation.x = Math.PI * -.5;
+        scene.add(mesh);
+      }
+      {
+        const cubeSize = 4;
+        const cubeGeo = new THREE.BoxBufferGeometry(cubeSize, cubeSize, cubeSize);
+        const cubeMat = new THREE.MeshPhongMaterial({color: '#8AC'});
+        const mesh = new THREE.Mesh(cubeGeo, cubeMat);
+        mesh.position.set(cubeSize + 1, cubeSize / 2, 0);
+        scene.add(mesh);
+      }
+      {
+        const sphereRadius = 3;
+        const sphereWidthDivisions = 32;
+        const sphereHeightDivisions = 16;
+        const sphereGeo = new THREE.SphereBufferGeometry(sphereRadius, sphereWidthDivisions, sphereHeightDivisions);
+        const sphereMat = new THREE.MeshPhongMaterial({color: '#CA8'});
+        const mesh = new THREE.Mesh(sphereGeo, sphereMat);
+        mesh.position.set(-sphereRadius - 1, sphereRadius + 2, 0);
+        scene.add(mesh);
+      }
+    
+      function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+          renderer.setSize(width, height, false);
+        }
+        return needResize;
+      }
+    
+      function render() {
+    
+        if (resizeRendererToDisplaySize(renderer)) {
+          const canvas = renderer.domElement;
+          camera.aspect = canvas.clientWidth / canvas.clientHeight;
+          camera.updateProjectionMatrix();
+        }
+    
+        renderer.render(scene, camera);
+    
+        requestAnimationFrame(render);
+      }
+    
+      requestAnimationFrame(render);
+    }
+    
+    main();
+    
